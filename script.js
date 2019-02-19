4e402487d0b1d38343b1d96f4b1ce761038051a9
codeParane <32712199+codeparane@users.noreply.github.com>
2019-02-19 21:56:27 +0530

    Exercise Continued..

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 function frmSubmit(){
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
-    var bloodType = document.getElementsByName("bloodType").value;
+    var bloodType = checkBloodType();
 
-    document.getElementById("display").innerHTML = "Hi Welcome " + firstName; 
+    document.getElementById("displayName").innerHTML = greetMsg(firstName, lastName);
+    document.getElementById("displayBloodComp").innerHTML = checkComp(bloodType);
+    
 }
+
+function greetMsg(fName, lName){
+    return "Hi Welcome " + fName + " " + lName;
+}
+
+function checkBloodType(){
+    if(document.getElementById("A").checked)
+        return "A";
+    else if(document.getElementById("O").checked)
+        return "O";
+}
+
+function checkComp(bloodType){
+    if(bloodType == "O")
+        return "You Can Donor For Anyone";  
+    else if(bloodType == "AB")
+        return "You can Receive From Anyone";
+    else if(bloodType == "A" || bloodType == "B")
+        return "You Should recive or donate for same Blood Group";
+    else    
+        return "There is no Compatablity";
+}
+
+
+
+
